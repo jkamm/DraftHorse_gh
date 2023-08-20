5160caefb74fb0c4b1ad1c05e7f0234a97a21f4d
jktesselate <jokamm@gmail.com>
2023-08-20 13:27:41 -0500

    Add project files.

---
+++ DraftHorseInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace DraftHorse
+{
+    public class DraftHorseInfo : GH_AssemblyInfo
+    {
+        public override string Name => "DraftHorse";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("ee57be77-5df8-4d13-b942-fa7177d208d1");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
